e68fc0c9d6be907f0c34d1b2013806a9c080f178
JamesCurtis <jamescurtis@foxmail.com>
2023-11-30 20:34:16 +0800

    fix: added BaseNgxOptionDto default value

--- src/portal/dto/base-ngx-option.dto.ts
+++ src/portal/dto/base-ngx-option.dto.ts
@@ -7,16 +7,16 @@ import { SeriesDto } from './series.dto';
 export abstract class BaseNgxOptionDto implements Partial<NgxOptions> {
   [key: string]: unknown;
 
-  @ApiProperty({ required: false, description: '是否显示标签' })
-  labels: boolean;
-  @ApiProperty({ required: false, description: '是否显示图例' })
-  legend: boolean;
-  @ApiProperty({ required: false, description: '显示网格线' })
-  showGridLines: boolean;
-  @ApiProperty({ required: false })
-  showXAxisLabel: boolean;
-  @ApiProperty({ required: false })
-  showYAxisLabel: boolean;
+  @ApiProperty({ required: false, default: false, example: true, description: '是否显示标签' })
+  labels: boolean = false;
+  @ApiProperty({ required: false, default: true, example: true, description: '是否显示图例' })
+  legend: boolean = true;
+  @ApiProperty({ required: false, default: false, example: true, description: '显示网格线' })
+  showGridLines: boolean = false;
+  @ApiProperty({ required: false, default: false, example: false })
+  showXAxisLabel: boolean = false;
+  @ApiProperty({ required: false, default: false, example: false })
+  showYAxisLabel: boolean = false;
   @ApiProperty({
     example: [1400, 800],
     default: [700, 300],
@@ -26,12 +26,12 @@ export abstract class BaseNgxOptionDto implements Partial<NgxOptions> {
     description: '图表大小（[宽，高]）',
   })
   view: [number, number];
-  @ApiProperty({ required: false, description: '是否显示 x 轴' })
-  xAxis: boolean;
+  @ApiProperty({ required: false, default: false, example: true, description: '是否显示 x 轴' })
+  xAxis: boolean = false;
   @ApiProperty({ example: 'test xAxisLabel', required: false })
   xAxisLabel: string;
-  @ApiProperty({ required: false, description: '是否显示 y 轴' })
-  yAxis: boolean;
+  @ApiProperty({ required: false, default: false, example: true, description: '是否显示 y 轴' })
+  yAxis: boolean = false;
   @ApiProperty({ example: 'test yAxisLabel', required: false })
   yAxisLabel: string;
 }
