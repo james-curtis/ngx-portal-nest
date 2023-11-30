12e933775dc9ceefa1ee403e2faeaa57152959de
JamesCurtis <jamescurtis@foxmail.com>
2023-11-30 21:00:07 +0800

    fix: 修改 swagger translator example

--- src/portal/dto/parse-input.dto.ts
+++ src/portal/dto/parse-input.dto.ts
@@ -14,16 +14,20 @@ import { AnyChartParamDto } from './any-chart-param.dto';
 export class ParseInputDto implements ParseChartParam {
   @ApiProperty({ type: AnyChartParamDto, description: '图表参数' })
   chartParam: AnyChartParam;
-  @ApiProperty({ required: false, enum: SeriesType, description: '数据 series 类型' })
-  seriesType: SeriesType = SeriesType.SingleSeries;
+  @ApiProperty({
+    required: false,
+    default: null,
+    nullable: true,
+    example: null,
+    enum: SeriesType,
+    description: '数据 series 类型（默认根据 chartParam.type 自动设置）',
+  })
+  seriesType: SeriesType = null;
   @ApiProperty({
     nullable: true,
     example: {
-      name: '$..bucketing_attributes[0]..value',
-      series: {
-        name: '$..bucketing_attributes[1]..value',
-        value: '$..counters..result.value',
-      },
+      name: '$..bucketing_attributes..value',
+      value: '$..counters..result.value',
     },
     required: false,
     description: 'jsonpath 对应关系',
