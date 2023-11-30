86be13cd07e664d3ba70bb4b853766c61e163149
JamesCurtis <jamescurtis@foxmail.com>
2023-11-30 21:12:33 +0800

    fix: 修改 swagger AnyChartParamDto example

--- src/portal/dto/any-chart-param.dto.ts
+++ src/portal/dto/any-chart-param.dto.ts
@@ -1,9 +1,16 @@
 import { ChartParamInputDto } from './chart-param-input.dto';
-import { AnyChartParam, AnyNgxOptions } from '../interfaces/portal.interface';
+import { AnyChartParam, AnyNgxOptions, ChartType } from '../interfaces/portal.interface';
 import { ApiProperty } from '@nestjs/swagger';
 import { AnyNgxOptionDto } from './any-ngx-option.dto';
 
 export class AnyChartParamDto extends ChartParamInputDto implements AnyChartParam {
+  @ApiProperty({
+    example: 'BarVerticalComponent',
+    default: 'BarVerticalComponent',
+    enum: ChartType,
+    description: '图表类型（ngx-charts 组件名）',
+  })
+  type: ChartType;
   @ApiProperty({ type: AnyNgxOptionDto, description: 'ngx-charts 组件参数' })
   ngxOptions: AnyNgxOptions;
 }
